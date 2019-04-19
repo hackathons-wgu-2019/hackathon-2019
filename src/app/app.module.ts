831ae366e399dcd3db12ff1ab19a5c3591250ac2
Sowmya Chaganti <sowmya.chaganti@wgu.edu>
2019-04-19 14:43:47 -0600

    added left, middle, right content comps, added app routing,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LeftContentComponent } from './left-content/left-content.component';
+import { MiddleContentComponent } from './middle-content/middle-content.component';
+import { RightContentComponent } from './right-content/right-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LeftContentComponent,
+    MiddleContentComponent,
+    RightContentComponent
   ],
   imports: [
     BrowserModule,
